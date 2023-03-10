5a47dc74c3026f06d482120fffc03b86c4c71b69
yansq <yan_sq@yeah.net>
2023-03-10 16:36:30 +0800

    Exercises 4.2

--- part4/index.js
+++ part4/index.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 const blogSchema = new mongoose.Schema({
     title: String,
@@ -11,28 +15,15 @@ const blogSchema = new mongoose.Schema({
     likes: Number
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const password = 'ZIiaLIREO3fbEJa0'
-const mongoUrl = `mongodb+srv://felzyan33:${password}@cluster0.mogjdll.mongodb.net/?retryWrites=true&w=majority`
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-app.get('/api/blogs', (request, response) => {
-    Blog.find({})
-        .then(blogs => response.json(blogs)
-    )
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-    blog.save()
-        .then(result => response.status(201).json(result))
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
---
+++ part4/utils/config.js
@@ -0,0 +1,8 @@
+const PORT = 3003
+const password = 'ZIiaLIREO3fbEJa0'
+const MONGODB_URI = `mongodb+srv://felzyan33:${password}@cluster0.mogjdll.mongodb.net/?retryWrites=true&w=majority`
+
+module.exports = {
+    MONGODB_URI,
+    PORT
+}
