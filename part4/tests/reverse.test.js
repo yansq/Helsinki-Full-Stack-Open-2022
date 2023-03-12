f0cabe5c95ecd436c5c2e7399d19512db7063e47
yansq <yan_sq@yeah.net>
2023-03-12 14:46:06 +0800

    Exercises 4.5

--- part4/tests/reverse.test.js
+++ part4/tests/reverse.test.js
@@ -43,3 +43,42 @@ describe('total likes', () => {
     expect(result).toBe(5)
   })
 })
+
+
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '422aa71b54a676234d17f8',
+      title: '15 Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 15,
+      __v: 0
+    },
+    {
+      _id: 'a422aa71b54a676234d17f8',
+      title: '4 Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 4,
+      __v: 0
+    }
+  ]
+
+  test('test if the function can find the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual({
+      title: '15 Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 15
+    })
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,10 +1,22 @@
 const dummy = (blogs) => 1
 
-const totalLikes = (blogs) => {
-    return blogs.reduce((sum, a) => sum + a.likes, 0)
+const totalLikes = (blogs) => blogs.reduce((sum, a) => sum + a.likes, 0)
+
+const favoriteBlog = (blogs) => {
+    let mostFavoriteBlog = blogs[0]
+    for (let i = 1; i < blogs.length; ++i) {
+        if (blogs[i].likes > mostFavoriteBlog.likes) {
+            mostFavoriteBlog = blogs[i]
+        }
+    }
+    delete mostFavoriteBlog._id
+    delete mostFavoriteBlog.url
+    delete mostFavoriteBlog.__v
+    return mostFavoriteBlog
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
