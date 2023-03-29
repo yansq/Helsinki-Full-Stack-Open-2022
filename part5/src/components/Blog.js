1904d48a43bafb9a4f972621054c0171019ae4ec
yansq <yan_sq@yeah.net>
2023-03-29 15:32:20 +0800

    Exercises 5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [isShowDetail, setIsShowDetail] = useState(false);
+
+  const hideWhenVisible = { display: isShowDetail ? "" : "none" };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "1px solid black",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setIsShowDetail(!isShowDetail)}>
+          {isShowDetail ? "hide" : "show"}
+        </button>
+      </div>
+      <div style={hideWhenVisible}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes ? blog.likes : 0}
+          <button>like</button>
+        </div>
+        <div>{blog.user ? blog.user.username : ""}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
