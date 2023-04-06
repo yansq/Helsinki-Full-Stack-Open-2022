cecc39707609b49befa1d013df70e7d7717c153f
yansq <yan_sq@yeah.net>
2023-04-06 15:50:26 +0800

    Exercises 6.9

--- part6/src/components/AnecdoteList.js
+++ part6/src/components/AnecdoteList.js
@@ -2,7 +2,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { doVote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) =>
+    state.filter === ""
+      ? state.anecdotes
+      : state.anecdotes.filter((a) => a.content.includes(state.filter))
+  );
   const dispatch = useDispatch();
 
   const vote = (id) => {
