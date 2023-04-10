47b8ac2b14628171c1fad66820120c640730ecf0
yansq <yan_sq@yeah.net>
2023-04-10 13:17:17 +0800

    Exercises 6.20

---
+++ part6-1/src/App.js
@@ -0,0 +1,43 @@
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { useQuery } from "react-query";
+import { getAnecdotes } from "./request";
+import axios from "axios";
+
+const App = () => {
+  const handleVote = (anecdote) => {
+    console.log("vote");
+  };
+
+  const result = useQuery("anecdotes", getAnecdotes);
+  console.log(result);
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
+  } else if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>;
+  }
+
+  const anecdotes = result.data;
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
